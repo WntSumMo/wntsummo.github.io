50af2d3746990f31a2f107e0263266c7166190b5
Rustam <45446306+WntSumMo@users.noreply.github.com>
2019-12-15 20:12:40 +0500

    Add files via upload

---
+++ GlobalDigitalAgency/js/main.js
@@ -0,0 +1,55 @@
+
+// scroll animation
+var animateButton = function(e) {
+
+  e.preventDefault;
+  //reset animation
+  e.target.classList.remove('animate');
+  
+  e.target.classList.add('animate');
+  setTimeout(function(){
+    e.target.classList.remove('animate');
+  },700);
+};
+
+
+
+// slider
+var bubblyButtons = document.getElementsByClassName("bubbly-button");
+
+for (var i = 0; i < bubblyButtons.length; i++) {
+  bubblyButtons[i].addEventListener('click', animateButton, false);
+}
+
+
+$(document).ready(function(){
+  $('.slider').slick({
+    
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    autoplay: true,
+  });
+});
+
+
+
+// navbar scrollup sticky 
+var header = $('.header'),
+	scrollPrev = 0;
+
+$(window).scroll(function() {
+	var scrolled = $(window).scrollTop();
+ 
+	if ( scrolled > 100 && scrolled > scrollPrev ) {
+		header.addClass('out');
+	} else {
+		header.removeClass('out');
+	}
+	scrollPrev = scrolled;
+});
+
+
+$('.menu-icon').click(function(){
+  $(this).toggleClass('clicked');
+});
