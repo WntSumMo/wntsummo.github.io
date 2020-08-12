f7fbeb136a9568ae4406ab6cd8e942aa39a87751
Rustam Ismailov <forceofgravity33@gmail.com>
2020-08-13 00:09:22 +0500

    v2

--- quantstyle-primary/js/main.js
+++ quantstyle-primary/js/main.js
@@ -86,48 +86,56 @@ $(function () {
   }
 
 
+  window.onscroll = function() {scrollFunction()};
 
-
-
-
-  // Hide Header on on scroll down
-  var didScroll;
-  var lastScrollTop = 0;
-  var delta = 5;
-  var navbarHeight = $('.header-navigation').outerHeight();
-
-  $(window).scroll(function (event) {
-    didScroll = true;
-  });
-
-  setInterval(function () {
-    if (didScroll) {
-      hasScrolled();
-      didScroll = false;
-    }
-  }, 250);
-
-  function hasScrolled() {
-    var st = $(this).scrollTop();
-
-    // Make sure they scroll more than delta
-    if (Math.abs(lastScrollTop - st) <= delta)
-      return;
-
-    // If they scrolled down and are past the navbar, add class .nav-up.
-    // This is necessary so you never see what is "behind" the navbar.
-    if (st > lastScrollTop && st > navbarHeight) {
-      // Scroll Down
-      $('.header-navigation').removeClass('nav-down').addClass('nav-up');
+  function scrollFunction() {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+      document.getElementById("logo").style.padding = "5px 130px 5px 0";
     } else {
-      // Scroll Up
-      if (st + $(window).height() < $(document).height()) {
-        $('.header-navigation').removeClass('nav-up').addClass('nav-down');
-      }
+      document.getElementById("logo").style.padding = "20px 130px 20px 0";
     }
-
-    lastScrollTop = st;
   }
+
+
+
+  // // Hide Header on on scroll down
+  // var didScroll;
+  // var lastScrollTop = 0;
+  // var delta = 5;
+  // var navbarHeight = $('.header-navigation').outerHeight();
+
+  // $(window).scroll(function (event) {
+  //   didScroll = true;
+  // });
+
+  // setInterval(function () {
+  //   if (didScroll) {
+  //     hasScrolled();
+  //     didScroll = false;
+  //   }
+  // }, 250);
+
+  // function hasScrolled() {
+  //   var st = $(this).scrollTop();
+
+  //   // Make sure they scroll more than delta
+  //   if (Math.abs(lastScrollTop - st) <= delta)
+  //     return;
+
+  //   // If they scrolled down and are past the navbar, add class .nav-up.
+  //   // This is necessary so you never see what is "behind" the navbar.
+  //   if (st > lastScrollTop && st > navbarHeight) {
+  //     // Scroll Down
+  //     $('.header-navigation').removeClass('nav-down').addClass('nav-up');
+  //   } else {
+  //     // Scroll Up
+  //     if (st + $(window).height() < $(document).height()) {
+  //       $('.header-navigation').removeClass('nav-up').addClass('nav-down');
+  //     }
+  //   }
+
+  //   lastScrollTop = st;
+  // }
 
 
   $('#data-protection').on('click', function() {
